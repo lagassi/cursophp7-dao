0e94ac7ba5424d27e4e86629f35db7b0f2e5e075
Willian Candido Lagassi <wilhan_cross@hotmai.com>
2017-09-21 11:30:49 -0400

    Inssert and Update

--- index.php
+++ index.php
@@ -22,12 +22,22 @@ echo json_encode($usuarios); */
 //echo json_encode($search);
 
 //Carrega um usuario usando login e a senha
+//$usuario = new Usuario();
+//$usuario->login("Jose", "1234567890");
+//echo $usuario;
 
+//Criando novo usuario
+//$aluno = new Usuario("aluno", "@alun0");
+//$aluno->insert();
+//echo $aluno;
+
+//Update de usuario
 $usuario = new Usuario();
-$usuario->login("Jose", "1234567890");
 
-echo $usuario;
+$usuario->loadById(8);
 
+$usuario->update("professor", "000999");
 
+echo $usuario
 
  ?>
